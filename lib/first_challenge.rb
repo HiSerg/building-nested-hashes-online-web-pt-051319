1f6ae71cb9e00e2aaeb21df954329a102c146c09
Sergio Guzman <hiserg98@gmail.com>
2019-06-19 01:52:58 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,24 +1,7 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {
-      :patriach => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo"
-        :age => "15"
-      }, 
-      :hero_friends
-   
-       
-       
-       
-       
-       
-     }
+epic_tragedy ={
+  :montegue => {},
+  :capulet => {}
+}
+
 end
